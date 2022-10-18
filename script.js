5c3a3c83594b5ff784d3418772d20e672078c9f7
eslamcoder <78663809+eslamcoder@users.noreply.github.com>
2022-10-18 19:55:58 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+/*var scroll = document.querySelector(".image img:nth-child(1)");
+window.addEventListener("scroll", function(){
+  var value = 1 + window.scrollY / -500;
+  scroll.style.transform =`top:${value}`;
+});*/
